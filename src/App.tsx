9249ed66df9b331f5e6421dfbedc6c011df736f9
Wanderson Chaves <wandersonscpibr@gmail.com>
2021-12-30 21:52:57 -0300

    testHour

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import RepositoryList from './components/RepositoryList';
+
+import store from './store';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <Provider store={store}>
+      <RepositoryList />
+    </Provider>
+  );
 }
 
 export default App;
